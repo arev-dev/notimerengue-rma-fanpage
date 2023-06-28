98262f1b76d8e186f29e61f4c13a42d8ca85c047
Daniel Rodriguez <arevdevgames@gmail.com>
2023-06-28 01:54:09 -0600

    [::white_check_mark: Add] CORS ADDED

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import noticiasRoutes from "./routes/noticia.routes.js"
+import cors from "cors"
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors({
+    origin: 'http://localhost:5174'
+  }));
 app.use(express.json());
 app.use(noticiasRoutes)
 
